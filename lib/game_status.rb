1a61f7e4b27373ee6a0e21ff73cb4c6e0b1af5de
Luan Devecchi <gitluan@engineer.com>
2018-08-28 18:29:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,3 +13,12 @@ WIN_COMBINATIONS = [
  [0,4,8],  # Diag1 row
  [6,4,2]  # Diag2 row
 ]
+
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+  end
+end
