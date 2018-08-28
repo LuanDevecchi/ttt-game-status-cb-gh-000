183c93393263e3a26e5c2b9c0d52f75877a96f8c
Luan Devecchi <gitluan@engineer.com>
2018-08-28 18:15:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,9 +7,9 @@ end
 
 WIN_COMBINATIONS = [
  [0,1,2], # Top row
- [3,4,5]  # Middle row
- [6,7,8]  # Bottom row
+ [3,4,5],  # Middle row
+ [6,7,8],  # Bottom row
 
- [0,4,8]  # Diag1 row
+ [0,4,8],  # Diag1 row
  [6,4,2]  # Diag2 row
 ]
